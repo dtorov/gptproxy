8a3c19292980abfd27c377ae6468058796d473c7
dtorov <dtorov@bk.ru>
2024-06-05 11:38:03 +0300

    ch ip addr

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const OpenAI = require('openai');
 
 const { port, token, openaiBotboomApiKey } = require("./config");
 const PORT = process.env.PORT || port;
+const HOST = process.env.PORT || host;
 const Token = process.env.PORT || token;
 const openai = new OpenAI({
     //apiKey: process.env['org-BNZkupLCFdvPqKGitiaSTFog'], // This is the default and can be omitted
@@ -14,7 +15,7 @@ const openai = new OpenAI({
 app.use(cors());
 app.use(Express.json());
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST || '127.0.0.1', () => {
     console.log("Server Listening on PORT:", port);
   });
 
